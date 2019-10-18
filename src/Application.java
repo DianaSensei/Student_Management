6b57366a9e0db213f6b1333550706238cb8142d2
DianaSensei <43933486+DianaSensei@users.noreply.github.com>
2019-10-18 12:22:15 +0700

    Update Application.java
    - New Command UI Step by step

--- src/Application.java
+++ src/Application.java
@@ -4,26 +4,52 @@ import java.util.Scanner;
 
 public class Application {
     Boolean isLogin;
-    private void Menu_Start(){
-        System.out.println(ConsoleColors.BLUE_BOLD + "Student Manager");
-        System.out.println(ConsoleColors.WHITE+"1. Load Student List from .CSV file");
-        System.out.println(ConsoleColors.WHITE+"2. Save Student List to .CSV file");
-        System.out.println(ConsoleColors.WHITE+"3. View Student List with ID");
-        System.out.println(ConsoleColors.WHITE+"4. View Student List with GPA");
-        System.out.println(ConsoleColors.WHITE+"5. Add new Student to List");
-        System.out.println(ConsoleColors.WHITE+"6. Delete Student from List");
-        System.out.println(ConsoleColors.WHITE+"7. Import/Export Student List from/to .CSV file");
+    private Manager_System manager_system ;
+    private Application(){
+        manager_system = new Manager_System();
     }
-
-    public static void main(String[] args) {
-        Application app = new Application();
-        Manager_System system = new Manager_System();
-        Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        int selected = 0;
-        boolean flag = false;
-        app.Menu_Start();
+    private void Menu_Start() {
         ConsoleColors.clearConsole();
-        do {
+        System.out.println(ConsoleColors.BLUE_BOLD + "Student Manager");
+        System.out.println(ConsoleColors.WHITE + "1. Load Student List from .CSV file");
+        System.out.println(ConsoleColors.WHITE + "2. Save Student List to .CSV file");
+        System.out.println(ConsoleColors.WHITE + "3. View Student List");
+        System.out.println(ConsoleColors.WHITE + "4. Add new Student to List");
+        System.out.println(ConsoleColors.WHITE + "5. Delete Student from List");
+        System.out.println(ConsoleColors.WHITE + "6. Import/Export Student List from/to .CSV file");
+        System.out.println(ConsoleColors.WHITE_BOLD+"8. Quit");
+        int selected = getIndexCommand(1, 6);
+        switch (selected) {
+                case 1: {
+                    System.out.println("Select " + selected);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Select " + selected);
+                    break;
+                }
+                case 3: {
+                    View_Menu();
+                    break;
+                }
+                case 4: {
+                    System.out.println("Select " + selected);
+                    break;
+                }
+                case 5: {
+                    System.out.println("Select " + selected);
+                    break;
+                }
+                case 6: {
+                    ImportExport_Menu();
+                    break;
+                }
+        }
+    }
+    private int getIndexCommand(int min, int max){
+        int selected = 0;boolean flag = false;
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        do{
             flag = false;
             System.out.println("Enter number of command: ");
             try {
@@ -34,38 +60,74 @@ public class Application {
                 flag = true;
                 System.out.println("Invalid command index!!");
             }
-            if(!flag)
-                switch (selected) {
-                    case 1: {
-                        System.out.println("Select " + selected);
-                        break;
-                    }
-                    case 2: {
-                        System.out.println("Select " + selected);
-                        break;
-                    }
-                    case 3: {
-                        System.out.println("Select " + selected);
-                        break;
-                    }
-                    case 4: {
-                        System.out.println("Select " + selected);
-                        break;
-                    }
-                    case 5: {
-                        System.out.println("Select " + selected);
-                        break;
-                    }
-                    case 6: {
-                        System.out.println("Select " + selected);
-                        break;
-                    }
-                    default: {
-                        ConsoleColors.clearConsole();
-                        flag = true;
-                        System.out.println("Invalid command index!!");
-                    }
-            }
-        } while (flag);
+            if(min <=selected || selected <= max) break;
+        }while (flag);
+        return selected;
+    }
+
+    private void View_Menu(){
+        ConsoleColors.clearConsole();
+        System.out.println(ConsoleColors.BLUE_BOLD + "Student Manager");
+        System.out.println(ConsoleColors.YELLOW_BOLD+"VIEW");
+        System.out.println(ConsoleColors.WHITE+"1. View Student List with ID");
+        System.out.println(ConsoleColors.WHITE+"2. View Student List with GPA");
+        System.out.println(ConsoleColors.WHITE_BOLD+"3. Back");
+        int selected = getIndexCommand(1,3);
+    }
+    private void ImportExport_Menu(){
+        ConsoleColors.clearConsole();
+        System.out.println(ConsoleColors.BLUE_BOLD + "Student Manager");
+        System.out.println(ConsoleColors.YELLOW_BOLD+"IMPORT/EXPORT");
+        System.out.println(ConsoleColors.WHITE+"1. Import Student List from .CSV file");
+        System.out.println(ConsoleColors.WHITE+"2. Export Student List to .CSV file");
+        System.out.println(ConsoleColors.WHITE_BOLD+"3. Back");
+        int selected = getIndexCommand(1,3);
+    }
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.Menu_Start();
+    }
+
+    private void Add(Scanner scanner){
+        System.out.println("***** ADD New Student *****\n");
+        Student student = new Student();
+        System.out.print("ID: ");
+        student.setID(scanner.nextLine());
+        System.out.print("Name: ");
+        student.setName(scanner.nextLine());
+        System.out.print("Address: ");
+        student.setAddress(scanner.nextLine());
+        System.out.print("GPA: ");
+        student.setGPA(scanner.nextDouble());
+        System.out.print("Note: ");
+        student.setNotes(scanner.nextLine());
+
+        if(manager_system.AddStudent(student)){
+            System.out.println("Add new Student Successfully");
+        }else {
+            System.out.println("Fail to Add Student!!");
+        }
+    }
+    private void Delete(Scanner scanner){
+        System.out.println("***** Remove New Student *****\n");
+        System.out.println("Enter Student ID: ");
+        if(manager_system.RemoveStudent_by_ID(scanner.nextLine())) {
+            System.out.println("Remove Successful!");
+        }else {
+            System.out.println("Fail to Remove Student!");
+        }
+    }
+    private void Import(){
+// TO DO
+    }
+    private void Export(){
+// TO DO
+    }
+    private void Save_to_File(){
+// TO DO
+    }
+    private void View(int type,boolean order){
+        if(type == 1 ) manager_system.View_by_GPA(order,System.out);
+        if(type == 0 ) manager_system.View_by_ID(order,System.out);
     }
 }
